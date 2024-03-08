867f3661d57a31b338a6a2d8acc044bb730f5026
huleeb <huleeb.digital@gmail.com>
2024-03-08 13:10:42 -0400

    resolve Easy decimate object #1

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ bl_info = {
     "name": "StupidGiant Toolkit",
     "author": "huleeb",
     "blender": (4, 0, 0),
-    "version": (0, 0, 2),
+    "version": (0, 0, 3),
     "category": "3D View"
 }
 
@@ -27,21 +27,69 @@ class ToggleOrbitAroundSelectionOperator(bpy.types.Operator):
         return {'FINISHED'}
     
 
+class EasyDecimate(bpy.types.Operator):
+	"""Decimate and hide new object"""
+	bl_idname = "toolkit.easy_decimate"
+	bl_label = "Decimate and hide new object"
+
+	def execute(self, context):
+		if bpy.context.selected_objects:
+			obj = bpy.context.selected_objects[0]
+            
+			new_obj = obj.copy()
+			new_obj.name = obj.name + "_decimated"
+			new_obj.data = obj.data.copy()
+			bpy.context.collection.objects.link(new_obj)
+            
+			obj.parent = new_obj
+			obj.display_type = 'BOUNDS'
+            
+			bpy.context.view_layer.objects.active = new_obj
+            
+			mod_dec = new_obj.modifiers.new(type='DECIMATE', name='Decimate')
+			new_obj.modifiers["Decimate"].ratio = 0.1
+			bpy.ops.object.modifier_apply(modifier=mod_dec.name)
+			bpy.ops.object.mode_set(mode='EDIT')
+			bpy.ops.mesh.select_all(action='SELECT')
+			bpy.ops.uv.smart_project(angle_limit=66)
+			bpy.ops.object.mode_set(mode='OBJECT')
+			
+			new_obj.visible_camera = False
+			new_obj.visible_diffuse = False
+			new_obj.visible_glossy = False
+			new_obj.visible_transmission = False
+			new_obj.visible_volume_scatter = False
+			new_obj.visible_shadow = False
+		return {'FINISHED'}
+
+
 addon_keymaps = []
 
 # Addon preference panel
-class KeymapPreferences(bpy.types.AddonPreferences):
-    bl_idname = __name__
+class AddonPreference(bpy.types.AddonPreferences):
+	bl_idname = __name__
     
-    def draw(self, context):
-        layout = self.layout
-        wm = bpy.context.window_manager
-        kc = wm.keyconfigs.addon
-        if kc:
-            layout.label(text="Shortcuts:", icon='OPTIONS')
-            for km, kmi in addon_keymaps:
-                row = layout.row()
-                rna_keymap_ui.draw_kmi([], kc, km, kmi, row, 0)
+	auto_check_update = bpy.props.BoolProperty(name="Auto-check for Update", description="If enabled, auto-check for updates using an interval", default=False)
+	updater_interval_months = bpy.props.IntProperty( name='Months', description="Number of months between checking for updates", default=0, min=0)
+	updater_interval_days = bpy.props.IntProperty( name='Days', description="Number of days between checking for updates", default=7, min=0, max=31)
+	updater_interval_hours = bpy.props.IntProperty( name='Hours', description="Number of hours between checking for updates", default=0, min=0, max=23)
+	updater_interval_minutes = bpy.props.IntProperty( name='Minutes', description="Number of minutes between checking for updates", default=0, min=0, max=59)
+    
+	def draw(self, context):
+		layout = self.layout
+		# Works best if a column, or even just self.layout.
+		mainrow = layout.row()
+		col = mainrow.column()
+
+		addon_updater_ops.update_settings_ui(self, context)
+          
+		wm = bpy.context.window_manager
+		kc = wm.keyconfigs.addon
+		if kc:
+			layout.label(text="Shortcuts:", icon='OPTIONS')
+			for km, kmi in addon_keymaps:
+				row = layout.row()
+				rna_keymap_ui.draw_kmi([], kc, km, kmi, row, 0)
 
 class UpdaterPanel(bpy.types.Panel):
 	"""Panel to popup notice and ignoring functionality"""
@@ -80,79 +128,17 @@ class UpdaterPanel(bpy.types.Panel):
 		addon_updater_ops.update_notice_box_ui(self, context)
 
 
-@addon_updater_ops.make_annotations
-class UpdaterPreferences(bpy.types.AddonPreferences):
-	"""bare-bones preferences"""
-	bl_idname = __package__
-
-	# Addon updater preferences.
-
-	auto_check_update = bpy.props.BoolProperty(
-		name="Auto-check for Update",
-		description="If enabled, auto-check for updates using an interval",
-		default=False)
-
-	updater_interval_months = bpy.props.IntProperty(
-		name='Months',
-		description="Number of months between checking for updates",
-		default=0,
-		min=0)
-
-	updater_interval_days = bpy.props.IntProperty(
-		name='Days',
-		description="Number of days between checking for updates",
-		default=7,
-		min=0,
-		max=31)
-
-	updater_interval_hours = bpy.props.IntProperty(
-		name='Hours',
-		description="Number of hours between checking for updates",
-		default=0,
-		min=0,
-		max=23)
-
-	updater_interval_minutes = bpy.props.IntProperty(
-		name='Minutes',
-		description="Number of minutes between checking for updates",
-		default=0,
-		min=0,
-		max=59)
-
-	def draw(self, context):
-		layout = self.layout
-
-		# Works best if a column, or even just self.layout.
-		mainrow = layout.row()
-		col = mainrow.column()
-
-		# Updater draw function, could also pass in col as third arg.
-		addon_updater_ops.update_settings_ui(self, context)
-
-		# Alternate draw function, which is more condensed and can be
-		# placed within an existing draw function. Only contains:
-		#   1) check for update/update now buttons
-		#   2) toggle for auto-check (interval will be equal to what is set above)
-		# addon_updater_ops.update_settings_ui_condensed(self, context, col)
-
-		# Adding another column to help show the above condensed ui as one column
-		# col = mainrow.column()
-		# col.scale_y = 2
-		# ops = col.operator("wm.url_open","Open webpage ")
-		# ops.url=addon_updater_ops.updater.website
-
 classes = (
-    ToggleOrbitAroundSelectionOperator,
-    KeymapPreferences,
+	AddonPreference,
 	UpdaterPanel,
-    UpdaterPreferences
+    ToggleOrbitAroundSelectionOperator,
+    EasyDecimate
 )
 
 def register():
     addon_updater_ops.register(bl_info)
 
     for cls in classes:
-        addon_updater_ops.make_annotations(cls)
         bpy.utils.register_class(cls)
     register_keymap()
     
@@ -176,7 +162,11 @@ def add_key_to_map(kc,config='3D View',space_type='VIEW_3D',cls='',key='',action
 def register_keymap():
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon.keymaps
+    
+	# Every addon shortcuts:
     add_key_to_map(kc,config='3D View',cls='toolkit.toggle_orbit',key='X',shift=True,alt=True,ctrl=True)
+    add_key_to_map(kc,config='3D View',cls='toolkit.easy_decimate',key='D',shift=True,alt=True,ctrl=True)
+    
 
 
 def unregister_keymaps():

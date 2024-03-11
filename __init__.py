da3018ddf795998ac977f03ed36b32f7d09365fd
huleeb <162403990+huleeb@users.noreply.github.com>
2024-03-11 15:47:34 -0300

    features for version 0.0.4 (#3)
    * add aspect ratio flip
    
    * started ctrl-m for mask modifier while overing modifier panel
    
    * add gray scale toggle
    
    * orbit toggle button now display current state
    
    * fix EasyDecimate object offset
    
    * change gray scale shortcut to F2
    
    * add undo feature
    
    * bump version to 0.0.4

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ bl_info = {
     "name": "Daily Toolkit",
     "author": "huleeb & stupidgiant",
     "blender": (4, 0, 0),
-    "version": (0, 0, 3),
+    "version": (0, 0, 4),
     "location": "View3D > Tools > daily",
     "category": "Generic",
     "description": "utility functions to speed up workflow"
@@ -16,13 +16,6 @@ bl_info = {
 #	- when mouse is over modifier window 
 #   	have shortcuts specific:
 #		- ctrl-m for mask modifier and create Vertex Group 'Group'
-#	- Toggle with F2 'Black and White' ColorRamp from compositor
-#
-#
-#
-#
-#
-#
 
 def disable_outline_options():
     for area in bpy.context.screen.areas:
@@ -40,7 +33,7 @@ def execute_outliner_filter_restricted():
             space.show_restrict_column_viewport = False
             space.show_restrict_column_select = False
 
-class outliner_filter_restricted(bpy.types.Operator):
+class OutlinerFilterRestricted(bpy.types.Operator):
     """Removes Viewport and Selectable restricted filters in outliner"""
     bl_label = "Outliner Filter"
     bl_idname = "toolkit.outliner_filter_restricted"
@@ -49,7 +42,7 @@ class outliner_filter_restricted(bpy.types.Operator):
         execute_outliner_filter_restricted()
         return {'FINISHED'}
 
-class flip_aspect_ratio(bpy.types.Operator):
+class FlipAspectRatio(bpy.types.Operator):
     """Toggles aspect ratio between portrait to landscape"""
     bl_label = "Flip Aspect Ratio"
     bl_idname = "toolkit.flip_aspect_ratio"
@@ -61,93 +54,164 @@ class flip_aspect_ratio(bpy.types.Operator):
         bpy.context.scene.render.resolution_y = x
         return {'FINISHED'}
 
+is_orbit_around_selection = False
+
 class ToggleOrbitAroundSelectionOperator(bpy.types.Operator):
-    """Toggle Orbit Around Selection"""
+    """Orbit Around Selection"""
     bl_idname = "toolkit.toggle_orbit"
-    bl_label = "Toggle Orbit Around Selection"
+    bl_label = "Orbit Around Selection"
 
     def execute(self, context):
+        global is_orbit_around_selection
         current_state = bpy.context.preferences.inputs.use_rotate_around_active
         
         bpy.context.preferences.inputs.use_rotate_around_active = not current_state
+
+        is_orbit_around_selection = not is_orbit_around_selection
+        bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
         
         if not current_state:
-            self.report({'INFO'}, 'Toggle Orbit Around Selection: ON')
+            self.report({'INFO'}, 'Orbit Around Selection: ON')
         else:
-            self.report({'INFO'}, 'Toggle Orbit Around Selection: OFF')
+            self.report({'INFO'}, 'Orbit Around Selection: OFF')
         return {'FINISHED'}
-    
 
 class EasyDecimate(bpy.types.Operator):
-	"""Decimate and hide new object"""
-	bl_idname = "toolkit.easy_decimate"
-	bl_label = "Decimate and hide new object"
+    """Decimate and hide new object"""
+    bl_idname = "toolkit.easy_decimate"
+    bl_label = "Decimate and hide new object"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    def execute(self, context):
+        if bpy.context.selected_objects:
+            obj = bpy.context.selected_objects[0]
+            
+            new_obj = obj.copy()
+            new_obj.name = obj.name + "_decimated"
+            new_obj.data = obj.data.copy()
+            bpy.context.collection.objects.link(new_obj)
+            
+            obj.parent = new_obj
+            obj.display_type = 'BOUNDS'
+            
+            # reset loc/rot of original obj, 
+            # if not, the object is offset by same amount on both values
+            obj.location = (0.0, 0.0, 0.0)
+            obj.rotation_euler = (0.0, 0.0, 0.0)
 
-	def execute(self, context):
-		if bpy.context.selected_objects:
-			obj = bpy.context.selected_objects[0]
+            bpy.context.view_layer.objects.active = new_obj
             
-			new_obj = obj.copy()
-			new_obj.name = obj.name + "_decimated"
-			new_obj.data = obj.data.copy()
-			bpy.context.collection.objects.link(new_obj)
+            mod_dec = new_obj.modifiers.new(type='DECIMATE', name='Decimate')
+            new_obj.modifiers["Decimate"].ratio = 0.1
+            bpy.ops.object.modifier_apply(modifier=mod_dec.name)
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.mesh.select_all(action='SELECT')
+            bpy.ops.uv.smart_project(angle_limit=66)
+            bpy.ops.object.mode_set(mode='OBJECT')
+            bpy.ops.object.shade_smooth()
             
-			obj.parent = new_obj
-			obj.display_type = 'BOUNDS'
-            
-			bpy.context.view_layer.objects.active = new_obj
-            
-			mod_dec = new_obj.modifiers.new(type='DECIMATE', name='Decimate')
-			new_obj.modifiers["Decimate"].ratio = 0.1
-			bpy.ops.object.modifier_apply(modifier=mod_dec.name)
-			bpy.ops.object.mode_set(mode='EDIT')
-			bpy.ops.mesh.select_all(action='SELECT')
-			bpy.ops.uv.smart_project(angle_limit=66)
-			bpy.ops.object.mode_set(mode='OBJECT')
-			
-			new_obj.visible_camera = False
-			new_obj.visible_diffuse = False
-			new_obj.visible_glossy = False
-			new_obj.visible_transmission = False
-			new_obj.visible_volume_scatter = False
-			new_obj.visible_shadow = False
-		return {'FINISHED'}
+            new_obj.visible_camera = False
+            new_obj.visible_diffuse = False
+            new_obj.visible_glossy = False
+            new_obj.visible_transmission = False
+            new_obj.visible_volume_scatter = False
+            new_obj.visible_shadow = False
+        return {'FINISHED'}
 
-class material_setting_to_bump_only(bpy.types.Operator):
+class MaterialSettingToBumpOnly(bpy.types.Operator):
     bl_label = "Bump Only"
     bl_idname = "toolkit.material_setting_to_bump_only"
+    bl_options = {'REGISTER', 'UNDO'}
 
-    def set_material_displacement_to_bump_only(material):
+    def set_material_displacement_to_bump_only(self,material):
         if material.use_nodes:
             principled_bsdf = material.node_tree.nodes.get("Principled BSDF")
             if principled_bsdf:
                 material.cycles.displacement_method = 'BUMP'
-                
-                # Remove existing displacement node if any
-                # displacement_node = None
-                # for node in material.node_tree.nodes:
-                #     if node.type == 'DISPLACEMENT':
-                #         displacement_node = node
-                #         break
-                # if displacement_node:
-                #     material.node_tree.nodes.remove(displacement_node)
     
     def execute(self, context):
         for obj in bpy.data.objects:
             if obj.type == 'MESH' and obj.data.materials:
                 for material_slot in obj.material_slots:
                     if material_slot.material:
-                        material_setting_to_bump_only.set_material_displacement_to_bump_only(material_slot.material)
+                        self.set_material_displacement_to_bump_only(material_slot.material)
         self.report({'INFO'}, 'Set all material displacement method to: Bump')
         return {'FINISHED'}
 
-addon_keymaps = []
+class ModifierMask(bpy.types.Operator):
+    """Adds a Mask modifier to every selected object. Along with an empty Vertex Group"""
+    bl_label = "Mask Modifier"
+    bl_idname = "toolkit.modifier_mask" 
+    bl_options = {'REGISTER', 'UNDO'}
+
+#     if bpy.context.space_data.context == 'MODIFIER':
+# AttributeError: 'SpaceView3D' object has no attribute 'context'
+
+    def execute(self, context):
+        if bpy.context.selected_objects:
+            try:
+                self.report({'INFO'}, str(bpy.context.selected_objects))
+                if bpy.context.space_data.context == 'MODIFIER':
+                    for obj in bpy.context.selected_objects:
+                        bpy.context.view_layer.objects.active = obj
+                        bpy.ops.object.vertex_group_add()
+                        mod_dec = obj.modifiers.new(type='MASK', name='Mask')
+                bpy.ops.ed.undo_push()
+            except:
+                pass
+        return {'FINISHED'}
+
+is_gray_scale = False
+
+class ToggleGrayScale(bpy.types.Operator):
+    """Gray Scale"""
+    bl_label = "Gray Scale"
+    bl_idname = "toolkit.gray_scale"
+
+    def execute(self, context):
+        bpy.context.space_data.shading.use_compositor = 'ALWAYS'
+        tree = context.scene.node_tree
+
+        composite_node = None
+        last_node = None
+        grayscale_node = None
+        grayscale_node_name = 'toolkit.grayscale'
+        global is_gray_scale
+
+        for node in tree.nodes:
+            if node.type == 'COMPOSITE':
+                composite_node = node
+                input_socket = node.inputs[0] 
+                if input_socket.is_linked:
+                    linked_socket = input_socket.links[0].from_socket
+                    last_node = linked_socket.node
+            if node.label == grayscale_node_name:
+                 grayscale_node = node
+
+        if grayscale_node == None:
+            color_ramp_node = tree.nodes.new('CompositorNodeValToRGB')
+            color_ramp_node.location = (last_node.location.x + 200, last_node.location.y)
+            color_ramp_node.label = color_ramp_node.name = grayscale_node_name
+
+            if last_node:
+                tree.links.new(last_node.outputs[0], color_ramp_node.inputs[0])
+            if composite_node:
+                tree.links.new(color_ramp_node.outputs[0], composite_node.inputs[0])
+        else:
+            grayscale_node.mute = not grayscale_node.mute
+            is_gray_scale = not is_gray_scale
+            bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
+
+        return {'FINISHED'}
+
+addon_keymaps_view3d = []
+addon_keymaps_properties = []
 
 # Addon preference panel
 @addon_updater_ops.make_annotations
 class AddonPreference(bpy.types.AddonPreferences):
 	bl_idname = __name__
-    
+     
 	auto_check_update = bpy.props.BoolProperty(name="Auto-check for Update", description="If enabled, auto-check for updates using an interval", default=False)
 	updater_interval_months = bpy.props.IntProperty( name='Months', description="Number of months between checking for updates", default=0, min=0)
 	updater_interval_days = bpy.props.IntProperty( name='Days', description="Number of days between checking for updates", default=7, min=0, max=31)
@@ -165,8 +229,12 @@ class AddonPreference(bpy.types.AddonPreferences):
 		wm = bpy.context.window_manager
 		kc = wm.keyconfigs.addon
 		if kc:
-			layout.label(text="Shortcuts:", icon='OPTIONS')
-			for km, kmi in addon_keymaps:
+			layout.label(text="Viewport:", icon='OPTIONS')
+			for km, kmi in addon_keymaps_view3d:
+				row = layout.row()
+				rna_keymap_ui.draw_kmi([], kc, km, kmi, row, 0)
+			layout.label(text="Properties:", icon='OPTIONS')
+			for km, kmi in addon_keymaps_properties:
 				row = layout.row()
 				rna_keymap_ui.draw_kmi([], kc, km, kmi, row, 0)
 
@@ -215,11 +283,18 @@ class DAILY_PT_toolkit_panel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("toolkit.outliner_filter_restricted")
+        layout.label(text="Viewport:")
         layout.operator("toolkit.material_setting_to_bump_only")
         layout.operator("toolkit.flip_aspect_ratio")
-        layout.operator("toolkit.toggle_orbit")
+        layout.operator("toolkit.toggle_orbit", depress=is_orbit_around_selection)
         layout.operator("toolkit.easy_decimate")
+
+        layout.label(text="Compositor:")
+        layout.operator("toolkit.gray_scale", depress=is_gray_scale)
+        
+        layout.label(text="Outliner:")
+        layout.operator("toolkit.outliner_filter_restricted")
+
 
 classes = (
 	AddonPreference,
@@ -227,9 +302,11 @@ classes = (
     ToggleOrbitAroundSelectionOperator,
     EasyDecimate,
     DAILY_PT_toolkit_panel,
-    material_setting_to_bump_only,
-    outliner_filter_restricted,
-    flip_aspect_ratio
+    MaterialSettingToBumpOnly,
+    OutlinerFilterRestricted,
+    FlipAspectRatio,
+    ModifierMask,
+    ToggleGrayScale
 )
 
 def register():
@@ -255,23 +332,34 @@ def add_key_to_map(kc,config='3D View',space_type='VIEW_3D',cls='',key='',action
     if km:
         km = kc.new(config, space_type=space_type)
     kmi = km.keymap_items.new(cls, key, action, shift=shift,alt=alt,ctrl=ctrl)
-    addon_keymaps.append((km, kmi))
+    if space_type == 'VIEW_3D':
+        addon_keymaps_view3d.append((km, kmi))
+    if space_type == 'EMPTY':
+        addon_keymaps_properties.append((km, kmi))
 
 def register_keymap():
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon.keymaps
-    
-	# Every addon shortcuts:
+
+	# Viewport shortcuts:
     add_key_to_map(kc,config='3D View',cls='toolkit.toggle_orbit',key='X',shift=True,alt=True,ctrl=True)
     add_key_to_map(kc,config='3D View',cls='toolkit.easy_decimate',key='D',shift=True,alt=True,ctrl=True)
     add_key_to_map(kc,config='3D View',cls='toolkit.outliner_filter_restricted',key='K',shift=False,alt=False,ctrl=False)
     add_key_to_map(kc,config='3D View',cls='toolkit.material_setting_to_bump_only',key='B',shift=True,alt=True,ctrl=True)
     add_key_to_map(kc,config='3D View',cls='toolkit.flip_aspect_ratio',key='P',shift=True,alt=True,ctrl=True)
+    add_key_to_map(kc,config='3D View',cls='toolkit.gray_scale',key='F2',shift=False,alt=False,ctrl=False)
+
+	# Properties shortcuts:
+    #add_key_to_map(kc,config='Object Mode',space_type='EMPTY',cls='toolkit.modifier_mask',key='M',shift=False,alt=False,ctrl=True)
+
 
 def unregister_keymaps():
-    for km, kmi in addon_keymaps:
+    for km, kmi in addon_keymaps_view3d:
         km.keymap_items.remove(kmi)
-    addon_keymaps.clear()
+    addon_keymaps_view3d.clear()
+    for km, kmi in addon_keymaps_properties:
+        km.keymap_items.remove(kmi)
+    addon_keymaps_properties.clear()
 
 if __name__ == "__main__":
     register()
